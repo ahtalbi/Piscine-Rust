fcd045265dad00b24c3f7eb3c48e598308e27616
ahtalbi <obetox1@gmail.com>
2026-06-26 07:54:01 +0100

    matrix_transposition

---
+++ matrix_transposition/src/lib.rs
@@ -0,0 +1,20 @@
+#[derive(Debug, PartialEq)]
+struct Matrix((i32, i32), (i32, i32));
+
+pub fn transpose(m: Matrix) -> Matrix {
+    Matrix(
+        (m.0.0, m.1.0),
+        (m.0.1, m.1.1)
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let matrix = Matrix((1, 3), (4, 5));
+        assert_eq!(transpose(matrix), Matrix((1, 4), (3, 5)));
+    }
+}
