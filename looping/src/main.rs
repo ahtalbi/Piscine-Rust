2a5149388b62fb6a610a1c38d3cab6317e3d8f2f
ahtalbi <obetox1@gmail.com>
2026-06-26 07:19:47 +0100

    looping

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     loop {
         let mut input = String::new();
         println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
-        io::stdin().read_line(&mut input);
+        io::stdin().read_line(&mut input).unwrap();
         input.pop();
         if input == "The letter e" {println!("Number of trials: {}", nt); break;}
         nt += 1;
