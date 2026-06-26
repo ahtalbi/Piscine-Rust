5cff1057e30ddd320e341f0aef37c574b4f36e27
ahtalbi <obetox1@gmail.com>
2026-06-26 06:39:24 +0100

    temperature_conv

--- temperature_conv/target/debug/deps/temperature_conv-c67dabeadec65136.d
+++
@@ -1,7 +0,0 @@
-/home/ahtalbi/Desktop/piscine-rust/temperature_conv/target/debug/deps/temperature_conv-c67dabeadec65136.d: src/lib.rs
-
-/home/ahtalbi/Desktop/piscine-rust/temperature_conv/target/debug/deps/libtemperature_conv-c67dabeadec65136.rlib: src/lib.rs
-
-/home/ahtalbi/Desktop/piscine-rust/temperature_conv/target/debug/deps/libtemperature_conv-c67dabeadec65136.rmeta: src/lib.rs
-
-src/lib.rs:
