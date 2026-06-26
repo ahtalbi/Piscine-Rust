c11ddf66cfd46ee3beeca5517db036f5c3468283
ahtalbi <obetox1@gmail.com>
2026-06-26 07:24:55 +0100

    looping

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
         println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
         io::stdin().read_line(&mut input).unwrap();
         input.pop();
-        if input == "The letter e" {println!("Number of trials: {}", nt); break;}
         nt += 1;
+        if input == "The letter e" {println!("Number of trials: {}", nt); break;}
     }
 }
