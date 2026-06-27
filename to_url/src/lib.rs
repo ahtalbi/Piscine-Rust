3ef201894a4ef04cd7ac0c5d4dd607f5ded67722
ahtalbi <obetox1@gmail.com>
2026-06-27 04:47:18 +0100

    doubtful

---
+++ to_url/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn to_url(s: &str) -> String {
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
