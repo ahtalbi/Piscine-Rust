1c9bb6641ace59d97b1d71badaae8e43b1892fa7
ahtalbi <obetox1@gmail.com>
2026-06-28 02:33:27 +0100

    nice concept learning structs that way i like this problem

--- circle/src/lib.rs
+++ circle/src/lib.rs
@@ -1,28 +1,28 @@
 #[derive(Debug, Clone, Copy)]
 pub struct Circle {
 	pub center : Point,
-	pub raduis : f64,
+	pub radius : f64,
 }
 
 impl Circle {
-    pub fn new(x: f64, y: f64, raduis: f64) -> Self {
+    pub fn new(x: f64, y: f64, radius: f64) -> Self {
         Circle {
             center: Point (x, y),
-            raduis: raduis,
+            radius: radius,
         }
     }
 
     pub fn diameter(&self) -> f64 {
-        self.raduis * 2.0
+        self.radius * 2.0
     }
     
     pub fn area(&self) -> f64 {
-        std::f64::consts::PI * self.raduis * self.raduis
+        std::f64::consts::PI * self.radius * self.radius
     }
 
     pub fn intersect(&self, circle: &Circle) -> bool {
         let dis = self.center.distance(circle.center);
-        dis <= self.raduis + circle.raduis
+        dis <= self.radius + circle.radius
     }
 }
 
@@ -30,9 +30,9 @@ impl Circle {
 pub struct Point (pub f64, pub f64);
 
 impl Point {
-    pub fn distance(&self, pointB: Point) -> f64 {
-        let dx = self.0 - pointB.0;
-        let dy = self.1 - pointB.1;
+    pub fn distance(&self, point_b: Point) -> f64 {
+        let dx = self.0 - point_b.0;
+        let dy = self.1 - point_b.1;
         (dx * dx + dy * dy).sqrt()
     }
 }
@@ -46,7 +46,7 @@ mod tests {
         let circle = Circle::new(500.0, 500.0, 150.0);
         let circle1 = Circle {
             center: Point(80.0, 115.0),
-            raduis: 30.0,
+            radius: 30.0,
         };
 
         let point_a = Point(1.0, 1.0);
