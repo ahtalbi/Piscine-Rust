009a6245f5f2ee2aedb1ac930da2fa0be90a52a3
ahtalbi <obetox1@gmail.com>
2026-07-04 01:32:49 +0100

    middle day

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -1,13 +1,13 @@
-use chrono::{Datelike, NaiveDate, Weekday};
+use chrono::{Datelike, NaiveDate};
 
 pub fn middle_day(year: u32) -> Option<chrono::Weekday> {
-    let date = NaiveDate::from_ymd_opt(year as i32, 1, 1).?;
+    let date = NaiveDate::from_ymd_opt(year as i32, 1, 1)?;
     
     if date.leap_year() {
         return None;
     }
 
-    Some(NaiveDate::from_yo_opt(year as i32, 183).?.weekday())
+    Some(NaiveDate::from_yo_opt(year as i32, 183)?.weekday())
 }
 
 #[cfg(test)]
