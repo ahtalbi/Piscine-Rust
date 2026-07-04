b147db076abb98ff3a1d95f067b94ddc0bfd3d46
ahtalbi <obetox1@gmail.com>
2026-07-04 01:41:09 +0100

    middle day

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -7,13 +7,3 @@ pub fn middle_day(year: u32) -> Option<chrono::Weekday> {
 
     Some(NaiveDate::from_yo_opt(year as i32, 183)?.weekday())
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        println!("{:?}", middle_day(1022));
-    }
-}
