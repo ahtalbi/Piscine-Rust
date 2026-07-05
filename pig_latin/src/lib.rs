6d370e7eac6c441ae97fd34f3cee83098a08c0dc
ahtalbi <obetox1@gmail.com>
2026-07-05 05:09:22 +0100

    pig_latin

--- pig_latin/src/lib.rs
+++ pig_latin/src/lib.rs
@@ -1,22 +1,24 @@
 pub fn pig_latin(text: &str) -> String {
     let vowels = ['a', 'e', 'i', 'o', 'u'];
     let chars: Vec<char> = text.chars().collect();
-    
+
     if vowels.contains(&chars[0]) {
         return format!("{}ay", text);
     }
 
     let mut i = 0;
+
     while i < chars.len() {
         if chars[i] == 'q' && i + 1 < chars.len() && chars[i + 1] == 'u' {
             i += 2;
+            continue;
         }
-        else if vowels.contains(&chars[i]) {
+
+        if vowels.contains(&chars[i]) {
             break;
         }
-        else {
-            i += 1;
-        }
+
+        i += 1;
     }
 
     let (head, tail) = text.split_at(i);
