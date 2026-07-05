20c0261e763d148ec1310792573be6a357050a1f
ahtalbi <obetox1@gmail.com>
2026-07-05 01:56:21 +0100

    searching

--- searching/src/lib.rs
+++ searching/src/lib.rs
@@ -10,8 +10,8 @@ mod tests {
     fn it_works() {
         let ar = [1, 3, 4, 6, 8, 9, 11, 8];
         let f = search(&ar, 8);
-        println!(
-            "the element 8 is last in the position {:?} in the array {:?}",
+        println!(ast i
+            "the element 8 is ln the position {:?} in the array {:?}",
             f, ar
         );
     }
