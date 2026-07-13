87a142e2010edba0b3a0e8d2ecce2bbbc3d03d69
ahtalbi <obetox1@gmail.com>
2026-07-13 15:35:01 +0100

    vectore operatoins

---
+++ vector_operations/src/lib.rs
@@ -0,0 +1,42 @@
+use std::ops::{Add, Sub};
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct ThreeDVector<T> {
+	pub i: T,
+	pub j: T,
+	pub k: T,
+}
+
+impl<T: std::ops::Add<Output = T>> Add for ThreeDVector<T> {
+    type Output = ThreeDVector<T>;
+    fn add(self, other: Self) -> Self::Output {
+        Self {
+            i: self.i + other.i,
+            j: self.j + other.j,
+            k: self.k + other.k,
+        }
+    }
+}
+
+impl<T: std::ops::Sub<Output = T>> Sub for ThreeDVector<T> {
+    type Output = ThreeDVector<T>;
+    fn sub(self, other: Self) -> Self::Output {
+        Self {
+            i: self.i - other.i,
+            j: self.j - other.j,
+            k: self.k - other.k,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let a = ThreeDVector { i: 3, j: 5, k: 2 };
+	    let b = ThreeDVector { i: 2, j: 7, k: 4 };
+	    println!("{:?}", a + b);
+    }
+}
